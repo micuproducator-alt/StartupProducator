4ea5e3e13f5789f2f15f670d5c9d75ad906a78d2
unknown <xandreix93@gmail.com>
2026-06-12 13:21:41 +0300

    Live map updated

--- frontend/components/LiveMap.tsx
+++ frontend/components/LiveMap.tsx
@@ -114,7 +114,7 @@ export const LiveMap: React.FC<LiveMapProps> = ({
         {/* ⚡️ AICI S-A FĂCUT UPGRADE-UL LA HARTA MODERNĂ CLARĂ */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
 
         {/* Ruta desenată */}
